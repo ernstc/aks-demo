1c1b616a36b6cf199f2bb347721bd5a1be4e28f5
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2021-06-22 16:11:19 +0200

    Removed obsolete files.

--- Scripts/secrets/aksvolume-prep.ps1
+++
@@ -1,9 +0,0 @@
-$accountName = "aksstorage001"
-$accountNameBytes = [System.Text.Encoding]::UTF8.GetBytes($accountName)
-$accountNameBase64 = [Convert]::ToBase64String($accountNameBytes)
-Write-Host "Account Name Base 64: " $accountNameBase64
-
-$accountKey = "Zm72XdnnrtgKioKpqb6hKxhF217iFWOUpxsn/DSmeEcwcHBVQaxYWkJEKGwqrYWYP6/GE2dCyRMiYPbM8JsGdw=="
-$accountKeyBytes = [System.Text.Encoding]::UTF8.GetBytes($accountKey)
-$accountKeyBase64 = [Convert]::ToBase64String($accountKeyBytes)
-Write-Host "Account Name Key 64: " $accountKeyBase64
--- Scripts/secrets/fileshare-setup.ps1
+++
@@ -1,9 +0,0 @@
-$connectTestResult = Test-NetConnection -ComputerName aksstorage001.file.core.windows.net -Port 445
-if ($connectTestResult.TcpTestSucceeded) {
-    # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"aksstorage001.file.core.windows.net`" /user:`"Azure\aksstorage001`" /pass:`"Zm72XdnnrtgKioKpqb6hKxhF217iFWOUpxsn/DSmeEcwcHBVQaxYWkJEKGwqrYWYP6/GE2dCyRMiYPbM8JsGdw==`""
-    # Mount the drive
-    New-PSDrive -Name W -PSProvider FileSystem -Root "\\aksstorage001.file.core.windows.net\aksvolume" -Persist
-} else {
-    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-}
